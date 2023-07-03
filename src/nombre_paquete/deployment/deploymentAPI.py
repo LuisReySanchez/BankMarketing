944e67996c336fdf34b1589765ec7896b7d4356f
LuisReySanchez <luen_2@yahoo.es>
2023-07-03 14:56:12 -0500

    Actulizacion Despliegue

--- src/nombre_paquete/deployment/deploymentAPI.py
+++ src/nombre_paquete/deployment/deploymentAPI.py
@@ -1,36 +1,45 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from typing import List
 import joblib
 import numpy as np
-import os
-import uvicorn
-# Cargar el modelo
-modelo = joblib.load('model_rf_GSearch.joblib')
+import logging
 
-# Definir la estructura de los datos de entrada
-class DatosEntrada(BaseModel):
-    features: list[float]
-    
-# Crear la instancia de la aplicación FastAPI
+# Estructura de los datos
+class ApiInput(BaseModel):
+    features: List[float]
+
+class ApiOutput(BaseModel):
+    is_deposito: List[int]
+        
+# Instancia de la aplicación FastAPI
 app = FastAPI()
 
-# Definir la ruta y el método para verificar si la aplicación está en funcionamiento
+# Carga del modelo
+modelo = joblib.load('model_rf_GSearch.joblib')
+
+# Configurar el logger
+logging.basicConfig(level=logging.ERROR)
+logger = logging.getLogger(__name__)
+
+# Raiz
 @app.get("/")
 def alive():
     return 'Alive'
 
 # Definir la ruta y el método para hacer predicciones
 @app.post("/predict")
-def predict(data: DatosEntrada):
-    # Convertir los datos de entrada en un array de NumPy
-    input_data = np.array([data.features], dtype=np.float32)
+async def create_user(data: ApiInput) -> ApiOutput:
 
-    # Realizar la predicción utilizando el modelo cargado
-    prediction = modelo.predict(input_data)
+    try:
+        input_data = np.array(data.features).reshape(1,20)
+        prediction = modelo.predict(input_data).flatten().tolist()
+        preds=ApiOutput(is_deposito=prediction)
 
-    # Devolver la predicción como resultado
-    return {"prediction": prediction[0]}
-
-# Iniciar el servidor de desarrollo con Uvicorn
-if __name__ == "__main__":
-    uvicorn.run(app, port=8000)
+        return preds
+    
+    except Exception as e:
+        logger.error(f"Error durante la predicción: {e}")
+        raise HTTPException(status_code=500, detail="Error interno del servidor")
+        print("error: ",e)
+        
